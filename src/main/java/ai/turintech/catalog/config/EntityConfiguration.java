d23f2350d1976fbb455b45886a1970f8453a9ef6
v.boursinos <v.boursinos@turintech.ai>
2023-11-01 10:14:22 +0000

    chore: dynamic way to scan annotations with reflection lib

--- src/main/java/ai/turintech/catalog/config/EntityConfiguration.java
+++ src/main/java/ai/turintech/catalog/config/EntityConfiguration.java
@@ -2,19 +2,17 @@ package ai.turintech.catalog.config;
 
 import ai.turintech.catalog.anotatation.Columns;
 import ai.turintech.catalog.anotatation.Relationship;
-import ai.turintech.catalog.domain.*;
+import ai.turintech.catalog.domain.Model;
 import ai.turintech.catalog.service.dto.GenericQueryDTO;
 import ai.turintech.catalog.service.dto.RelationshipDTO;
 import ai.turintech.catalog.service.dto.TableInfoDTO;
+import org.reflections.Reflections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.relational.core.sql.Table;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Configuration
 public class EntityConfiguration {
@@ -48,67 +46,49 @@ public class EntityConfiguration {
     }
 
     @Bean
-    public GenericQueryDTO scanPackageForTableInfo() {
+    public GenericQueryDTO scanPackageForTableInfo2() {
         GenericQueryDTO genericQueryDTO = new GenericQueryDTO();
         List<TableInfoDTO> tableInfoList = new ArrayList<>();
+
         Map<String, List<String>> tableColumnMap = new HashMap<>();
-        for (Object instance : getInstances()) {
-            TableInfoDTO tableInfoDTO = new TableInfoDTO();
-            List<String> columnList = new ArrayList<>();
-            List<RelationshipDTO> relationshipDTOList = new ArrayList<>();
-            String tableName = null;
-            if (instance.getClass().isAnnotationPresent(org.springframework.data.relational.core.mapping.Table.class)) {
-                tableName = instance.getClass().getAnnotation(org.springframework.data.relational.core.mapping.Table.class).value();
-                tableInfoDTO.setTable(Table.aliased(tableName, instance.getClass().getSimpleName()));
-                tableInfoDTO.setColumnPrefix(instance.getClass().getSimpleName());
-            }
-            if (instance.getClass().isAnnotationPresent(Columns.class)) {
-                Columns columns = instance.getClass().getAnnotation(Columns.class);
+        Reflections reflections = new Reflections("ai.turintech.catalog.domain");
+
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Columns.class);
+        for (Class<?> clazz : annotated) {
+            if (clazz.isAnnotationPresent(Columns.class)) {
+                String tableName = null;
+                TableInfoDTO tableInfoDTO = new TableInfoDTO();
+                if (clazz.isAnnotationPresent(org.springframework.data.relational.core.mapping.Table.class)) {
+                    tableName = clazz.getAnnotation(org.springframework.data.relational.core.mapping.Table.class).value();
+                    tableInfoDTO.setTable(Table.aliased(tableName, clazz.getSimpleName()));
+                    tableInfoDTO.setColumnPrefix(clazz.getSimpleName());
+                }
+                Columns columns = clazz.getAnnotation(Columns.class);
+                List<String> columnList = new ArrayList<>();
+                List<RelationshipDTO> relationshipDTOList = new ArrayList();
+
                 for (String column : columns.names()) {
-                    System.out.println(column);
                     columnList.add(column);
                 }
+
                 tableInfoDTO.setColumns(columnList);
                 tableColumnMap.put(tableName, columnList);
-            }
-            for (Field field : instance.getClass().getDeclaredFields()) {
-                System.out.println(field.getName());
-                Relationship relationship = field.getAnnotation(Relationship.class);
-                if (relationship == null) {
-                    continue;
+
+                for (Field field : clazz.getDeclaredFields()) {
+                    Relationship relationship = field.getAnnotation(Relationship.class);
+                    if (relationship != null) {
+                        Table tableObj = Table.aliased(relationship.toTable(), relationship.toColumnPrefix());
+                        RelationshipDTO relationshipDTO = new RelationshipDTO(relationship.type(), relationship.toTable(), tableObj, relationship.fromColumn(), relationship.toColumn(), relationship.toColumnPrefix());
+                        relationshipDTOList.add(relationshipDTO);
+                    }
                 }
-                Table tableObj = Table.aliased(relationship.toTable(), relationship.toColumnPrefix());
-                RelationshipDTO relationshipDTO = new RelationshipDTO(relationship.type(), relationship.toTable(), tableObj, relationship.fromColumn(), relationship.toColumn(), relationship.toColumnPrefix());
-                relationshipDTOList.add(relationshipDTO);
+
+                tableInfoDTO.setRelationships(relationshipDTOList);
+                tableInfoList.add(tableInfoDTO);
             }
-            tableInfoDTO.setRelationships(relationshipDTOList);
-            tableInfoList.add(tableInfoDTO);
         }
         genericQueryDTO.setTables(tableInfoList);
         genericQueryDTO.setTableColumnsMap(tableColumnMap);
         return genericQueryDTO;
     }
-
-
-    private List<Object> getInstances() {
-        Model model = new Model();
-        MlTaskType mlTaskType = new MlTaskType();
-        ModelType modelType = new ModelType();
-        ModelEnsembleType modelEnsembleType = new ModelEnsembleType();
-        ModelFamilyType modelFamilyType = new ModelFamilyType();
-        ModelStructureType modelStructureType = new ModelStructureType();
-        ModelGroupType modelGroupType = new ModelGroupType();
-        ModelGroupsRel modelGroupsRel = new ModelGroupsRel();
-        List<Object> instances = new ArrayList<>();
-        instances.add(model);
-        instances.add(mlTaskType);
-        instances.add(modelType);
-        instances.add(modelEnsembleType);
-        instances.add(modelFamilyType);
-        instances.add(modelStructureType);
-        instances.add(modelGroupType);
-        instances.add(modelGroupsRel);
-        return instances;
-    }
-
 }
