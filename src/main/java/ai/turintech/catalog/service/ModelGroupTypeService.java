e9de3f8d0fd306343880e376446bb400953cf7bf
v.boursinos <v.boursinos@turintech.ai>
2023-11-07 16:33:29 +0000

    chore: use one callable for each entity modelgroupType

--- src/main/java/ai/turintech/catalog/service/ModelGroupTypeService.java
+++ src/main/java/ai/turintech/catalog/service/ModelGroupTypeService.java
@@ -1,17 +1,22 @@
 package ai.turintech.catalog.service;
 
+import ai.turintech.catalog.callable.ModelGroupTypeCallable;
 import ai.turintech.catalog.domain.ModelGroupType;
 import ai.turintech.catalog.repository.ModelGroupTypeRepository;
 import ai.turintech.catalog.service.dto.ModelGroupTypeDTO;
 import ai.turintech.catalog.service.mapper.ModelGroupTypeMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -24,14 +29,17 @@ public class ModelGroupTypeService {
 
     private final Logger log = LoggerFactory.getLogger(ModelGroupTypeService.class);
 
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
+    private Scheduler jdbcScheduler;
+
+    @Autowired
     private ModelGroupTypeRepository modelGroupTypeRepository;
 
+    @Autowired
     private ModelGroupTypeMapper modelGroupTypeMapper;
-
-//    public ModelGroupTypeService(ModelGroupTypeRepository modelGroupTypeRepository, ModelGroupTypeMapper modelGroupTypeMapper) {
-//        this.modelGroupTypeRepository = modelGroupTypeRepository;
-//        this.modelGroupTypeMapper = modelGroupTypeMapper;
-//    }
 
     /**
      * Save a modelGroupType.
@@ -39,11 +47,10 @@ public class ModelGroupTypeService {
      * @param modelGroupTypeDTO the entity to save.
      * @return the persisted entity.
      */
-    public ModelGroupTypeDTO save(ModelGroupTypeDTO modelGroupTypeDTO) {
+    public Mono<ModelGroupTypeDTO> save(ModelGroupTypeDTO modelGroupTypeDTO) {
         log.debug("Request to save ModelGroupType : {}", modelGroupTypeDTO);
-        ModelGroupType modelGroupType = modelGroupTypeMapper.toEntity(modelGroupTypeDTO);
-        modelGroupType = modelGroupTypeRepository.save(modelGroupType);
-        return modelGroupTypeMapper.toDto(modelGroupType);
+        ModelGroupTypeCallable callable = context.getBean(ModelGroupTypeCallable.class, "create", modelGroupTypeDTO);
+        return Mono.fromCallable(callable).publishOn(jdbcScheduler);
     }
 
     /**
@@ -52,11 +59,10 @@ public class ModelGroupTypeService {
      * @param modelGroupTypeDTO the entity to save.
      * @return the persisted entity.
      */
-    public ModelGroupTypeDTO update(ModelGroupTypeDTO modelGroupTypeDTO) {
+    public Mono<ModelGroupTypeDTO> update(ModelGroupTypeDTO modelGroupTypeDTO) {
         log.debug("Request to update ModelGroupType : {}", modelGroupTypeDTO);
-        ModelGroupType modelGroupType = modelGroupTypeMapper.toEntity(modelGroupTypeDTO);
-        modelGroupType = modelGroupTypeRepository.save(modelGroupType);
-        return modelGroupTypeMapper.toDto(modelGroupType);
+        ModelGroupTypeCallable callable = context.getBean(ModelGroupTypeCallable.class, "update", modelGroupTypeDTO);
+        return Mono.fromCallable(callable).publishOn(jdbcScheduler);
     }
 
     /**
@@ -65,18 +71,10 @@ public class ModelGroupTypeService {
      * @param modelGroupTypeDTO the entity to update partially.
      * @return the persisted entity.
      */
-    public Optional<ModelGroupTypeDTO> partialUpdate(ModelGroupTypeDTO modelGroupTypeDTO) {
+    public Mono<ModelGroupTypeDTO> partialUpdate(ModelGroupTypeDTO modelGroupTypeDTO) {
         log.debug("Request to partially update ModelGroupType : {}", modelGroupTypeDTO);
-
-        return modelGroupTypeRepository
-            .findById(modelGroupTypeDTO.getId())
-            .map(existingModelGroupType -> {
-                modelGroupTypeMapper.partialUpdate(existingModelGroupType, modelGroupTypeDTO);
-
-                return existingModelGroupType;
-            })
-            .map(modelGroupTypeRepository::save)
-            .map(modelGroupTypeMapper::toDto);
+        ModelGroupTypeCallable callable = context.getBean(ModelGroupTypeCallable.class, "partialUpdate", modelGroupTypeDTO);
+        return Mono.fromCallable(callable).publishOn(jdbcScheduler);
     }
 
     /**
@@ -85,13 +83,20 @@ public class ModelGroupTypeService {
      * @return the list of entities.
      */
     @Transactional(readOnly = true)
-    public List<ModelGroupTypeDTO> findAll() {
+    public Mono<List<ModelGroupTypeDTO>> findAll() {
         log.debug("Request to get all ModelGroupTypes");
-        return modelGroupTypeRepository
-            .findAll()
-            .stream()
-            .map(modelGroupTypeMapper::toDto)
-            .collect(Collectors.toCollection(LinkedList::new));
+        ModelGroupTypeCallable callable = context.getBean(ModelGroupTypeCallable.class, "findAll");
+        return Mono.fromCallable(callable).publishOn(jdbcScheduler);
+    }
+
+    @Transactional(readOnly = true)
+    public Flux<ModelGroupTypeDTO> findAllStream() {
+        log.debug("Request to get all ModelGroupTypes");
+        return Flux.fromIterable(modelGroupTypeRepository
+                .findAll()
+                .stream()
+                .map(modelGroupTypeMapper::toDto)
+                .collect(Collectors.toCollection(LinkedList::new)));
     }
 
     /**
@@ -101,9 +106,10 @@ public class ModelGroupTypeService {
      * @return the entity.
      */
     @Transactional(readOnly = true)
-    public Optional<ModelGroupTypeDTO> findOne(UUID id) {
+    public Mono<ModelGroupTypeDTO> findOne(UUID id) {
         log.debug("Request to get ModelGroupType : {}", id);
-        return modelGroupTypeRepository.findById(id).map(modelGroupTypeMapper::toDto);
+        ModelGroupTypeCallable callable = context.getBean(ModelGroupTypeCallable.class, "findById", id);
+        return Mono.fromCallable(callable).publishOn(jdbcScheduler);
     }
 
     /**
@@ -111,8 +117,15 @@ public class ModelGroupTypeService {
      *
      * @param id the id of the entity.
      */
-    public void delete(UUID id) {
+    public Mono<Void> delete(UUID id) {
         log.debug("Request to delete ModelGroupType : {}", id);
-        modelGroupTypeRepository.deleteById(id);
+        ModelGroupTypeCallable callable = context.getBean(ModelGroupTypeCallable.class, "delete", id);
+        Mono delete = Mono.fromCallable(callable);
+        delete.subscribe(result -> {
+            System.out.println(result);
+        }, error -> {
+            System.err.println("An error occurred: " + error.toString());
+        });
+        return delete.subscribeOn(jdbcScheduler);
     }
 }
--- src/main/java/ai/turintech/catalog/web/rest/ModelGroupTypeResource.java
+++ src/main/java/ai/turintech/catalog/web/rest/ModelGroupTypeResource.java
@@ -6,24 +6,21 @@ import ai.turintech.catalog.service.dto.ModelGroupTypeDTO;
 import ai.turintech.catalog.web.rest.errors.BadRequestAlertException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tech.jhipster.web.util.HeaderUtil;
-import tech.jhipster.web.util.ResponseUtil;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 /**
  * REST controller for managing {@link ai.turintech.catalog.domain.ModelGroupType}.
@@ -39,14 +36,11 @@ public class ModelGroupTypeResource {
     @Value("${jhipster.clientApp.name}")
     private String applicationName;
 
+    @Autowired
     private ModelGroupTypeService modelGroupTypeService;
 
+    @Autowired
     private ModelGroupTypeRepository modelGroupTypeRepository;
-
-//    public ModelGroupTypeResource(ModelGroupTypeService modelGroupTypeService, ModelGroupTypeRepository modelGroupTypeRepository) {
-//        this.modelGroupTypeService = modelGroupTypeService;
-//        this.modelGroupTypeRepository = modelGroupTypeRepository;
-//    }
 
     /**
      * {@code POST  /model-group-types} : Create a new modelGroupType.
@@ -57,22 +51,25 @@ public class ModelGroupTypeResource {
      */
     @PostMapping("/model-group-types")
     public Mono<ResponseEntity<ModelGroupTypeDTO>> createModelGroupType(@Valid @RequestBody ModelGroupTypeDTO modelGroupTypeDTO)
-        throws URISyntaxException {
+            throws URISyntaxException {
         log.debug("REST request to save ModelGroupType : {}", modelGroupTypeDTO);
         if (modelGroupTypeDTO.getId() != null) {
             throw new BadRequestAlertException("A new modelGroupType cannot already have an ID", ENTITY_NAME, "idexists");
         }
-        ModelGroupTypeDTO result = modelGroupTypeService.save(modelGroupTypeDTO);
-        return Mono.just(ResponseEntity
-                .created(new URI("/api/model-group-types/" + result.getId()))
-                .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
-                .body(result));
+        Mono<ModelGroupTypeDTO> result = modelGroupTypeService.save(modelGroupTypeDTO);
+        return result
+                .map(
+                        res -> ResponseEntity
+                                .ok()
+                                .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, res.getId().toString()))
+                                .body(res)
+                );
     }
 
     /**
      * {@code PUT  /model-group-types/:id} : Updates an existing modelGroupType.
      *
-     * @param id the id of the modelGroupTypeDTO to save.
+     * @param id                the id of the modelGroupTypeDTO to save.
      * @param modelGroupTypeDTO the modelGroupTypeDTO to update.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated modelGroupTypeDTO,
      * or with status {@code 400 (Bad Request)} if the modelGroupTypeDTO is not valid,
@@ -81,8 +78,8 @@ public class ModelGroupTypeResource {
      */
     @PutMapping("/model-group-types/{id}")
     public Mono<ResponseEntity<ModelGroupTypeDTO>> updateModelGroupType(
-        @PathVariable(value = "id", required = false) final UUID id,
-        @Valid @RequestBody ModelGroupTypeDTO modelGroupTypeDTO
+            @PathVariable(value = "id", required = false) final UUID id,
+            @Valid @RequestBody ModelGroupTypeDTO modelGroupTypeDTO
     ) throws URISyntaxException {
         log.debug("REST request to update ModelGroupType : {}, {}", id, modelGroupTypeDTO);
         if (modelGroupTypeDTO.getId() == null) {
@@ -96,17 +93,20 @@ public class ModelGroupTypeResource {
             throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
         }
 
-        ModelGroupTypeDTO result = modelGroupTypeService.update(modelGroupTypeDTO);
-        return Mono.just(ResponseEntity
-                .ok()
-                .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, modelGroupTypeDTO.getId().toString()))
-                .body(result));
+        Mono<ModelGroupTypeDTO> result = modelGroupTypeService.update(modelGroupTypeDTO);
+        return result
+                .map(
+                        res -> ResponseEntity
+                                .ok()
+                                .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, res.getId().toString()))
+                                .body(res)
+                );
     }
 
     /**
      * {@code PATCH  /model-group-types/:id} : Partial updates given fields of an existing modelGroupType, field will ignore if it is null
      *
-     * @param id the id of the modelGroupTypeDTO to save.
+     * @param id                the id of the modelGroupTypeDTO to save.
      * @param modelGroupTypeDTO the modelGroupTypeDTO to update.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated modelGroupTypeDTO,
      * or with status {@code 400 (Bad Request)} if the modelGroupTypeDTO is not valid,
@@ -114,10 +114,10 @@ public class ModelGroupTypeResource {
      * or with status {@code 500 (Internal Server Error)} if the modelGroupTypeDTO couldn't be updated.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
-    @PatchMapping(value = "/model-group-types/{id}", consumes = { "application/json", "application/merge-patch+json" })
+    @PatchMapping(value = "/model-group-types/{id}", consumes = {"application/json", "application/merge-patch+json"})
     public Mono<ResponseEntity<ModelGroupTypeDTO>> partialUpdateModelGroupType(
-        @PathVariable(value = "id", required = false) final UUID id,
-        @NotNull @RequestBody ModelGroupTypeDTO modelGroupTypeDTO
+            @PathVariable(value = "id", required = false) final UUID id,
+            @NotNull @RequestBody ModelGroupTypeDTO modelGroupTypeDTO
     ) throws URISyntaxException {
         log.debug("REST request to partial update ModelGroupType partially : {}, {}", id, modelGroupTypeDTO);
         if (modelGroupTypeDTO.getId() == null) {
@@ -131,12 +131,15 @@ public class ModelGroupTypeResource {
             throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
         }
 
-        Optional<ModelGroupTypeDTO> result = modelGroupTypeService.partialUpdate(modelGroupTypeDTO);
+        Mono<ModelGroupTypeDTO> result = modelGroupTypeService.partialUpdate(modelGroupTypeDTO);
 
-        return Mono.justOrEmpty(tech.jhipster.web.util.ResponseUtil.wrapOrNotFound(
-                result,
-                HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, modelGroupTypeDTO.getId().toString()))
-        );
+        return result
+                .map(
+                        res -> ResponseEntity
+                                .ok()
+                                .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, res.getId().toString()))
+                                .body(res)
+                );
     }
 
     /**
@@ -147,17 +150,18 @@ public class ModelGroupTypeResource {
     @GetMapping(value = "/model-group-types", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<List<ModelGroupTypeDTO>> getAllModelGroupTypes() {
         log.debug("REST request to get all ModelGroupTypes");
-        return Mono.justOrEmpty(modelGroupTypeService.findAll());
+        return modelGroupTypeService.findAll();
     }
 
     /**
      * {@code GET  /model-group-types} : get all the modelGroupTypes as a stream.
+     *
      * @return the {@link Flux} of modelGroupTypes.
      */
     @GetMapping(value = "/model-group-types", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<ModelGroupTypeDTO> getAllModelGroupTypesAsStream() {
         log.debug("REST request to get all ModelGroupTypes as a stream");
-        return Flux.fromIterable(modelGroupTypeService.findAll());
+        return modelGroupTypeService.findAllStream();
     }
 
     /**
@@ -169,8 +173,10 @@ public class ModelGroupTypeResource {
     @GetMapping("/model-group-types/{id}")
     public Mono<ResponseEntity<ModelGroupTypeDTO>> getModelGroupType(@PathVariable UUID id) {
         log.debug("REST request to get ModelGroupType : {}", id);
-        Optional<ModelGroupTypeDTO> modelGroupTypeDTO = modelGroupTypeService.findOne(id);
-        return Mono.justOrEmpty(ResponseUtil.wrapOrNotFound(modelGroupTypeDTO));
+        Mono<ModelGroupTypeDTO> modelGroupTypeDTO = modelGroupTypeService.findOne(id);
+        return modelGroupTypeDTO.map(
+                modelGroupType -> ResponseEntity.ok().body(modelGroupType)
+        );
     }
 
     /**
@@ -183,9 +189,11 @@ public class ModelGroupTypeResource {
     public Mono<ResponseEntity<Void>> deleteModelGroupType(@PathVariable UUID id) {
         log.debug("REST request to delete ModelGroupType : {}", id);
         modelGroupTypeService.delete(id);
-        return Mono.justOrEmpty(ResponseEntity
-                .noContent()
-                .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
-                .build());
+        return Mono.just(
+                ResponseEntity
+                        .noContent()
+                        .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
+                        .build()
+        );
     }
 }
